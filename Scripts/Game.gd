f1901ca06b28893bd3bcfe286bdcf06351ee4d53
Nicolas C <nicolas.cachera@ext.adeo.com>
2022-05-29 19:28:17 +0200

    Add gameover music

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -6,30 +6,22 @@ onready var timer_score = $TimerScore
 
 onready var music_intro = $Music/AudioStreamPlayer_Intro
 onready var music_main = $Music/AudioStreamPlayer_Main
-onready var music_gameover = $MusicAudioStreamPlayer_GameOver
+onready var music_gameover = $Music/AudioStreamPlayer_GameOver
 
 var score : int
 
 func _ready():
 	score = 0
 	music_intro.play()
+	MainScript.connect("game_over_signal", self, "_on_game_over")
 
+func _on_game_over():
+	music_gameover.play()
+	
 func _on_Timer_timeout():
 	MainScript.score += 1
 	print(score)
 	emit_signal("update_score", score)
-	
-func game_over():
-	#timer_score.stop()
-	#tree.paused = true
-	
-	if music_intro.playing:
-		music_intro.stop()
-	if music_main.playing:
-		music_main.stop()
-	music_gameover.play()
-	
-	emit_signal("game_over_signal")
 	
 func _on_AudioStreamPlayer_Intro_finished():
 	music_main.play()
