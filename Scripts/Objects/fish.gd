4ce897deb066f578af9337967f6fe53cfbdcc21c
cam <MARQUETTE>
2022-05-29 19:17:15 +0200

    moving fish

--- Scripts/Objects/fish.gd
+++ Scripts/Objects/fish.gd
@@ -4,13 +4,16 @@ export var minSpeed : float = 50
 export var maxSpeed : float = 80
 
 var speed : float = 0.0
+var xPosition : float = 0.0
+var counter : float = 0.0
 
 func _ready():
 	speed = rand_range(minSpeed, maxSpeed)
 
 func _physics_process(delta):
 	self.position.y += speed * delta
+	self.position.x -= speed * delta
 
-
+	
 func _on_Rock_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	pass
