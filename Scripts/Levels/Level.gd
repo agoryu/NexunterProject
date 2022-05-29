e68605044cc99f2a914ad252ef08e6b8ccd7d09a
cam <MARQUETTE>
2022-05-29 18:15:22 +0200

    add fish + resize rock

--- Scripts/Levels/Level.gd
+++ Scripts/Levels/Level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var rock := preload("res://Scenes/Objects/Rock.tscn")
+onready var fish := preload("res://Scenes/Objects/Poisson.tscn")
 
 onready var path := $ObjectSpawner/PathFollow2D
 
@@ -9,4 +10,7 @@ func _on_Timer_timeout():
 	path.offset = randi()
 	var new_rock = rock.instance()
 	new_rock.position = path.position
+	var new_fish = fish.instance()
+	new_fish.position = path.position
+	add_child(new_fish)
 	add_child(new_rock)
