a52569c322d18d84472f1e97a03fc77a7c7a75d8
agoryu <elliot.vanegue@protonmail.com>
2022-06-04 21:14:21 +0200

    add the save score

--- Scripts/MainScript.gd
+++ Scripts/MainScript.gd
@@ -3,8 +3,12 @@ extends Node
 signal game_over_signal
 
 onready var tree := get_tree()
+onready var CustomSortScore = preload("res://Scripts/Sort/CustomSortScore.gd")
+onready var SAVE_PATH = "res://scores.json"
+onready var save_file = File.new()
 
 var score : int
+var scores = Dictionary()
 
 func _ready():
 	score = 0
@@ -12,3 +16,34 @@ func _ready():
 func game_over():
 	tree.paused = true
 	emit_signal("game_over_signal")
+	
+func load_data():
+	if not save_file.file_exists(SAVE_PATH):
+		print("ERROR: file does not exist (res://scores.json)")
+		return 
+	
+	save_file.open(SAVE_PATH, File.READ)
+	scores = parse_json(save_file.get_as_text())
+	save_file.close()
+	
+func save_score(score: int, player_name: String):
+	if scores == null or scores.empty():
+		scores[player_name + String(score)] = [score, player_name]
+	else:
+		var score_list = scores.values()
+		score_list.sort_custom(CustomSortScore, "sort_ascending")
+		var previous_score = []
+		for old_score in score_list:
+			if score > old_score[0]:
+				previous_score = old_score
+			else:
+				break
+		
+		if previous_score != []:
+			if scores.size() > 9:
+				scores.erase(score_list[0][1] + String(score_list[0][0]))
+			scores[player_name + String(score)] = [score, player_name]
+	
+	save_file.open(SAVE_PATH, File.WRITE)
+	save_file.store_line(to_json(scores))
+	save_file.close()
---
+++ Scripts/Screens/NameSaver.gd
@@ -0,0 +1,53 @@
+extends Control
+
+var accepted = false
+var letter_selected = 0
+var label_selected = 0
+var player_name = []
+
+onready var button_ok = $SaveButton
+onready var animation = $NameLetter/AnimationPlayer
+onready var timer = $NameLetter/Timer
+onready var letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+onready var label = [$NameLetter/Label, $NameLetter/Label2, $NameLetter/Label3]
+
+func _ready():
+	self.set_process_unhandled_input(false)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_up"):
+		set_letter(letter_selected + 1)
+	if event.is_action_pressed("ui_down"):
+		set_letter(letter_selected - 1)
+	if event.is_action_released("ui_accept"):
+		if label_selected == 2:
+			player_name.push_back(letter[letter_selected])
+			button_ok.visible = true
+			button_ok.grab_focus()
+		else:
+			label_selected += 1
+			player_name.push_back(letter[letter_selected])
+			letter_selected = 0
+			label[label_selected - 1].valign = 1
+		animation.stop()
+	if event.is_action_released("ui_back"):
+		if label_selected != 0:
+			label_selected -= 1
+			player_name.push_back(letter[letter_selected])
+			
+func _process(delta):
+	if !animation.is_playing():
+		animation.play(String(label_selected))
+
+func _on_NameSaver_visibility_changed():
+	self.set_process_unhandled_input(visible)
+
+func _on_SaveButton_button_up():
+	self.visible = false
+	var player_name_label = player_name[0] + player_name[1] + player_name[2]
+	MainScript.save_score(MainScript.score, player_name_label)
+
+func set_letter(letter_position):
+	letter_selected = fmod(letter_position, 26) 
+	label[label_selected].text = letter[letter_selected]
+	label[label_selected].grab_focus()
